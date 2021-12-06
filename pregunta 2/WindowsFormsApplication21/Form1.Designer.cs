06783c130b33194527239bf9c17656b061e3781d
juanmamanihumerez <91488752+juanmamanihumerez@users.noreply.github.com>
2021-12-06 05:50:09 -0400

    Add files via upload

---
+++ pregunta 2/WindowsFormsApplication21/Form1.Designer.cs
@@ -0,0 +1,293 @@
+﻿namespace WindowsFormsApplication21
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben eliminar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.button8 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox1.Location = new System.Drawing.Point(18, 18);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(800, 712);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseClick);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(830, 18);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(202, 35);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Recuperar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(882, 108);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(148, 26);
+            this.textBox1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(882, 148);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(148, 26);
+            this.textBox2.TabIndex = 4;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(882, 188);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(148, 26);
+            this.textBox3.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(850, 112);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(21, 20);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "R";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(850, 152);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(22, 20);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "G";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(850, 192);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(20, 20);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "B";
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox2.Location = new System.Drawing.Point(1041, 18);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(800, 712);
+            this.pictureBox2.TabIndex = 9;
+            this.pictureBox2.TabStop = false;
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(834, 303);
+            this.button7.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(202, 35);
+            this.button7.TabIndex = 14;
+            this.button7.Text = "Guardar región";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(830, 63);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(202, 35);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Color (10,10)";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(834, 267);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(198, 26);
+            this.textBox4.TabIndex = 15;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(834, 436);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(198, 26);
+            this.textBox5.TabIndex = 16;
+            
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(834, 543);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(196, 35);
+            this.button8.TabIndex = 17;
+            this.button8.Text = "Recuperar textura";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Black",
+            "Blue",
+            "Cyan",
+            "Green",
+            "Gray",
+            "White",
+            "Magenta",
+            "Yellow",
+            "Red",
+            "Orange"});
+            this.comboBox1.Location = new System.Drawing.Point(834, 494);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(198, 28);
+            this.comboBox1.TabIndex = 18;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(837, 239);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(151, 20);
+            this.label4.TabIndex = 19;
+            this.label4.Text = "Nombre de la region";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(832, 407);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(127, 20);
+            this.label5.TabIndex = 20;
+            this.label5.Text = "Region Buscada";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(833, 471);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(123, 20);
+            this.label6.TabIndex = 21;
+            this.label6.Text = "Color de Región";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1868, 751);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ pregunta 2/WindowsFormsApplication21/Form1.cs
@@ -0,0 +1,355 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Data.SqlClient;
+namespace WindowsFormsApplication21
+{
+    public partial class Form1 : Form
+    {
+        int cR, cG, cB;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
+            Bitmap bmp = new Bitmap(openFileDialog1.FileName);
+            pictureBox1.Image = bmp;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Bitmap bmp = new Bitmap(pictureBox1.Image);
+            Color c = new Color();
+            c = bmp.GetPixel(10,10);
+            textBox1.Text = c.R.ToString();
+            //textBox2.Text = c.G.ToString();
+            textBox3.Text = c.B.ToString();
+        }
+
+        private void pictureBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+            /*Bitmap bmp = new Bitmap(pictureBox1.Image);
+            Color c = new Color();
+            c = bmp.GetPixel(e.X, e.Y);
+            textBox1.Text = c.R.ToString();
+            textBox2.Text = c.G.ToString();
+            textBox3.Text = c.B.ToString();
+            cR = c.R;
+            cG = c.G;
+            cB = c.B;*/
+            Bitmap bmp = new Bitmap(pictureBox1.Image);
+            Color c = new Color();
+            int x, y, mR=0, mG=0, mB=0;
+            x = e.X; y = e.Y;
+            for (int i = x; i < x + 10;i++)
+                for (int j = y; j < y + 10; j++)
+                {
+                    c = bmp.GetPixel(i, j);
+                    mR = mR + c.R;
+                    mG = mG + c.G;
+                    mB = mB + c.B;
+                }
+            mR = mR / 100;
+            mG = mG / 100;
+            mB = mB / 100;
+            cR = mR;
+            cG = mG;
+            cB = mB;
+            textBox1.Text = cR.ToString();
+            textBox2.Text = cG.ToString();
+            textBox3.Text = cB.ToString();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Bitmap bmp = new Bitmap(pictureBox1.Image);
+            Bitmap cpoa = new Bitmap(bmp.Width, bmp.Height);
+            Color c = new Color();
+            for (int i = 1; i < bmp.Width; i++)
+                for (int j = 1; j < bmp.Height; j++)
+                {
+                    c = bmp.GetPixel(i, j);
+                    cpoa.SetPixel(i, j, c);
+                }
+            pictureBox2.Image = cpoa;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Bitmap bmp = new Bitmap(pictureBox1.Image);
+            Bitmap cpoa = new Bitmap(bmp.Width, bmp.Height);
+            Color c = new Color();
+            for (int i = 1; i < bmp.Width; i++)
+                for (int j = 1; j < bmp.Height; j++)
+                {
+                    c = bmp.GetPixel(i, j);
+                    cpoa.SetPixel(i, j, Color.FromArgb(c.R, 0 , 0) );
+                }
+            pictureBox2.Image = cpoa;
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            Bitmap bmp = new Bitmap(pictureBox1.Image);
+            Bitmap cpoa = new Bitmap(bmp.Width, bmp.Height);
+            Color c = new Color();
+            for (int i = 1; i < bmp.Width; i++)
+                for (int j = 1; j < bmp.Height; j++)
+                {
+                    c = bmp.GetPixel(i, j);
+                    cpoa.SetPixel(i, j, Color.FromArgb(0, c.G, 0));
+                }
+            pictureBox2.Image = cpoa;
+        }
+
+        public void guardaregion(string cod, string reg, string color, int cordx, int cordy)
+        {
+            SqlConnection con = new SqlConnection("server=(local);user=usuario324;pwd=123456;database=juanmamanihumerez");
+            con.Open();
+            SqlCommand cmd = new SqlCommand("insert into region values(@cod, @reg, @color,@cordx,@cordy)", con);
+            cmd.Parameters.AddWithValue("@cod", cod);
+            cmd.Parameters.AddWithValue("@reg", reg);
+            cmd.Parameters.AddWithValue("@color", color);
+            cmd.Parameters.AddWithValue("@cordx", cordx);
+            cmd.Parameters.AddWithValue("@cordy", cordy);
+            cmd.ExecuteNonQuery();
+
+            con.Close();
+        }
+
+        public void recuperaDatos()
+        {
+            SqlConnection con = new SqlConnection("server=(local);user=usuario324;pwd=123456;database=juanmamanihumerez");
+            con.Open();
+            SqlCommand cmd = new SqlCommand("SELECT * FROM region WHERE cod=@cod", con);
+            cmd.Parameters.AddWithValue("@cod", "R1");
+
+            SqlDataReader registro = cmd.ExecuteReader();
+
+        }
+        private void button8_Click(object sender, EventArgs e)
+        {
+            //int meR, meG, meB;
+            Bitmap bmp = new Bitmap(pictureBox1.Image);
+            Bitmap cpoa = new Bitmap(bmp.Width, bmp.Height);
+            Color c = new Color();
+            string cod = textBox5.Text;
+            string color = comboBox1.Text;
+            
+            SqlConnection con = new SqlConnection("server=(local);user=usuario324;pwd=123456;database=juanmamanihumerez");
+            con.Open();
+            SqlCommand cmd = new SqlCommand("SELECT * FROM region WHERE cod=@cod", con);
+            cmd.Parameters.AddWithValue("@cod",cod);
+            
+            SqlDataReader registro = cmd.ExecuteReader();
+         
+
+            while (registro.Read())
+            {   
+                if(registro["reg"].ToString().Equals("z1"))
+                {
+                    cpoa.SetPixel((int)registro["datosx"], (int)registro["datosy"], ColorTranslator.FromHtml(color));
+                }
+                else
+                { 
+                    c = bmp.GetPixel((int)registro["datosx"], (int)registro["datosy"]);
+                    cpoa.SetPixel((int)registro["datosx"], (int)registro["datosy"], c);
+                }
+            }
+            pictureBox2.Image = cpoa;
+            con.Close();
+            
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            Bitmap bmp = new Bitmap(pictureBox1.Image);
+            Bitmap cpoa = new Bitmap(bmp.Width, bmp.Height);
+            Color c = new Color();
+            for (int i = 1; i < bmp.Width; i++)
+                for (int j = 1; j < bmp.Height; j++)
+                {
+                    c = bmp.GetPixel(i, j);
+                    if (((cR - 10) < c.R) && (c.R < (cR + 10)) && ((cG - 10) < c.G) && (c.G < (cG + 10)) && ((cB - 10) < c.B) && (c.B < (cB + 10)))
+                        cpoa.SetPixel(i, j, Color.Black);
+                    else 
+                       cpoa.SetPixel(i, j, c);
+                }
+            pictureBox2.Image = cpoa;
+           
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            int meR, meG, meB;
+            
+            Bitmap bmp = new Bitmap(pictureBox1.Image);
+            Bitmap cpoa = new Bitmap(bmp.Width, bmp.Height);
+            Color c = new Color();
+            for (int i = 0; i < bmp.Width - 10; i += 10)
+                for (int j = 0; j < bmp.Height - 10; j += 10)
+                {
+                    meR = 0;
+                    meG = 0;
+                    meB = 0;
+
+                    for (int k = i; k < i + 10; k++)
+                    
+                        for (int l = j; l < j + 10; l++)
+                        {
+                            c = bmp.GetPixel(k, l);
+                            meR = meR + c.R;
+                            meG = meG + c.G;
+                            meB = meB + c.B;
+                        }
+                        meR = meR / 100;
+                        meG = meG / 100;
+                        meB = meB / 100;
+
+
+                    /*string strx = "0";
+                    string stry = "0";
+                    string totalx = "0";
+                    string totaly = "0";*/
+
+
+                    List<int> lrx = new List<int>();
+                    List<int> lry = new List<int>();
+
+                    List<int> lcx = new List<int>();
+                    List<int> lcy = new List<int>();
+                    
+
+                    if (((cR - 10) < meR) && (meR < (cR + 10)) && ((cG - 10) < meG) && (meG < (cG + 10)) && ((cB - 10) < meB) && (meB < (cB + 10)))
+                        
+                            for (int k = i; k < i + 10; k++)
+                            
+                                for (int l = j; l < j + 10; l++)
+                                {
+                                //strx = strx + "," + k;
+                                //stry = stry + "," + l;
+                                cpoa.SetPixel(k, l, Color.Orange);
+                                string reg="z1";
+                                    
+                                //lrx.Add(k);
+                                //lry.Add(l);
+                                guardaregion(textBox4.Text,reg, textBox5.Text, k, l);
+                            }
+                            
+                            /*strx = strx.Remove(0, 2);
+                            stry = stry.Remove(0, 2);
+                            */
+                            //guardaregion(textBox4.Text, textBox5.Text, strx, stry);
+
+                            /*string[] valoresx = strx.Split(',');
+                            string[] valoresy = stry.Split(',');
+
+                            int[] arregloIntx = new int[valoresx.Length];
+                            int[] arregloInty = new int[valoresy.Length];
+
+                            for (int px = 0; px < valoresx.Length; px++)
+                            {
+                                arregloIntx[px] = Int32.Parse(valoresx[px]);
+                            }
+
+                            for (int py = 0; py < valoresy.Length; py++)
+                            {
+                                arregloInty[py] = Int32.Parse(valoresy[py]);
+                            }
+
+                            for (int x = 0; x < arregloIntx.Length; x++)
+                            {
+                                for (int y = 0; y < arregloInty.Length; y++)
+                                {
+                                    cpoa.SetPixel(arregloIntx[x], arregloInty[y], Color.Orange);
+                                }
+                            }*/
+
+                        
+                        else
+                        
+                            for (int k = i; k < i + 10; k++)
+                            
+                                for (int l = j; l < j + 10; l++)
+                                {
+                                c = bmp.GetPixel(k, l);
+                                cpoa.SetPixel(k, l, c);
+
+                                //totalx = totalx + "," + k;
+                                //totaly = totaly + "," + l;
+                                string reg = "z2";
+                                //lcx.Add(k);
+                                //lcy.Add(l);
+                                guardaregion(textBox4.Text, reg, textBox5.Text, k, l);
+
+                            }
+
+                    /*
+                    totalx = totalx.Remove(0, 2);
+                    totaly = totaly.Remove(0, 2);
+
+                    Console.WriteLine(totalx);
+                    //guardaregion(textBox4.Text, textBox5.Text, strx, stry);
+
+                    string[] valx = totalx.Split(',');
+                    string[] valy = totaly.Split(',');
+
+                    int[] tarregloIntx = new int[valx.Length];
+                    int[] tarregloInty = new int[valy.Length];
+
+                    for (int px = 0; px < valx.Length; px++)
+                    {
+                        tarregloIntx[px] = Int32.Parse(valx[px]);
+
+                    }
+
+                    for (int py = 0; py < valy.Length; py++)
+                    {
+                        tarregloInty[py] = Int32.Parse(valy[py]);
+
+                    }
+
+                    for (int x = 0; x < tarregloIntx.Length; x++)
+                    {
+                        for (int y = 0; y < tarregloInty.Length; y++)
+                        {
+
+                            c = bmp.GetPixel(tarregloIntx[x], tarregloInty[y]);
+                            cpoa.SetPixel(tarregloIntx[x], tarregloInty[y], c);
+                        }
+                    }*/
+                    
+                    for (int x = 0; x < lrx.Count; x++)
+
+                        for (int y = 0; y < lry.Count; y++)
+                        {
+                            //c = bmp.GetPixel(lrx[x], lry[y]);
+                            //cpoa.SetPixel(lrx[x], lry[y], c);
+                            cpoa.SetPixel(lrx[x], lry[y], Color.Orange);
+                        }
+
+                    for (int x = 0; x < lcx.Count; x++)
+
+                        for (int y = 0; y < lcy.Count; y++)
+                        {
+                            c = bmp.GetPixel(lcx[x], lcy[y]);
+                            cpoa.SetPixel(lcx[x], lcy[y], c);
+                        }
+                    
+                }
+            pictureBox2.Image = cpoa;
+        }
+    }
+}
