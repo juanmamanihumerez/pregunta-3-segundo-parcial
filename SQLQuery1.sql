0e6ea637c3637abc950ae24cef8e6d1441b1de0e
juanmamanihumerez <91488752+juanmamanihumerez@users.noreply.github.com>
2021-12-06 08:35:18 -0400

    Add files via upload

---
+++ SQLQuery1.sql
@@ -0,0 +1,17 @@
+create table region
+(
+	cod varchar(2),
+	reg varchar(2),
+	color varchar(10),
+	datosx int,
+	datosy int	
+)
+
+select * from region
+
+SELECT * FROM region WHERE cod='R1'
+
+DROP TABLE region
+
+delete from region
+
